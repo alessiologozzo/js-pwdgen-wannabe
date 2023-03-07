9a76f4a1e7493e7dc0ba6714590b9453f039e09f
alessiologozzo <alessio.logozzo@libero.it>
2023-03-07 13:32:42 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual è il tuo nome?");
+let cognome = prompt("E il tuo cognome?");
+let colore = prompt("Ora dimmi il tuo colore preferito");
+
+document.write(nome + cognome + colore + 21);
